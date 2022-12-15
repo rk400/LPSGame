53ac7b92e891deafb4b25b5aaa97b6de014affe8
Ruth Rodriguez <rrs999@inlumine.ual.es>
2022-12-15 19:07:31 +0100

    Level 2 creado

--- Assets/Scripts/FruitsManager.cs
+++ Assets/Scripts/FruitsManager.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FruitsManager : MonoBehaviour
 {
+    private void Update(){
+        AllFruitsCollected();
+    }
     public void AllFruitsCollected() {
-        if (transform.childCount==1) {
+        if (transform.childCount==0) {
             Debug.Log("All Fruits Collected. You Win!");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
