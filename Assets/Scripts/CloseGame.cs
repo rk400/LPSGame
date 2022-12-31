2c1a6ea55cef3576fc3aeb4de16adf5cd8498912
Ruth Rodriguez <rrs999@inlumine.ual.es>
2022-12-31 13:05:29 +0100

    Ultimos cambios

--- Assets/Scripts/CloseGame.cs
+++ Assets/Scripts/CloseGame.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class CloseGame : MonoBehaviour
 {
+    public GameObject transition;
+
     void Start(){}
     void Update(){}
     public void Exit()
     {
+        transition.SetActive(true);
         Application.Quit();
         Debug.Log("Game is exiting");
     }
